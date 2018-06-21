8e1d554b648c3dffb33566c236511da6b21f62e6
JecaTatu <gas5@cin.ufpe.br>
2018-06-21 15:58:40 -0300

    Css do formulário pronto

---
+++ static/js/form.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var form = document.getElementById("contatoform");
+    $('#contatoform').prepend($('<div id="divaux2"></div>'));
+    $('#contatoform').prepend($('<div id="divaux1"></div>'));
+    $('#divaux1').append(form.children[2]);
+    $('#divaux1').append(form.children[2]);
+    $('#divaux1').append(form.children[3]);
+    $('#divaux1').append(form.children[2]);
+    $('#divaux1').append(form.children[4]);
+    $('#divaux2').append(form.children[2]);
+    $('#divaux2').append(form.children[2]);
+    $('#divaux2').append(form.children[2]);
+});
